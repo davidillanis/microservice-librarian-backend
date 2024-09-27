74feaa835c56f4762187ca1d3444f3eba840d2aa
davidillanis <brayanillanis@gmail.com>
2024-09-27 11:57:40 -0500

    -

--- microservice-issue/src/main/java/org/microservice/issue/MicroserviceIssueApplication.java
+++ microservice-issue/src/main/java/org/microservice/issue/MicroserviceIssueApplication.java
@@ -1,15 +1,24 @@
 package org.microservice.issue;
 
+import org.microservice.issue.service.implementation.AddDataServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MicroserviceIssueApplication {
+public class MicroserviceIssueApplication implements CommandLineRunner {
+	@Autowired
+	private AddDataServiceImpl addDataService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceIssueApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		addDataService.addData();
+	}
 }
